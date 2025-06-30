0d6edf86da4bbde8bd2deb4e1c635af4f5f92d01
ChiBao <baotochi87@gmail.com>
2025-06-30 07:52:35 +0700

    chuong2

---
+++ ch02/variable.go
@@ -0,0 +1,83 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println(`Greetings
+	// and Salutations`)
+
+	// var flag bool
+	// var isHandsome int = 1
+
+	// fmt.Println("flag is: ", flag)
+	// fmt.Println("isHandsome: ", isHandsome)
+
+	// var a int;
+	// var y float64;
+	// var z float64 = float64(a) + y;
+	// var d int = a + int(y)
+
+	// var x = 10
+	// fmt.Println(x)
+
+	// var x, y int = 10, 20
+	// fmt.Println(x)
+	// fmt.Println(y)
+
+	// var x, y = 10, "Hello World"
+	// fmt.Println(x)
+	// fmt.Println(y)
+
+	// var (
+	// 	x    int
+	// 	y        = 20
+	// 	z    int = 30
+	// 	d, e     = 40, "Hello"
+	// )
+	// fmt.Println(x)
+	// fmt.Println(y)
+	// fmt.Println(z)
+	// fmt.Println(d)
+	// fmt.Println(e)
+
+	// var x = 10
+	// y := 20
+
+	// fmt.Println(x)
+	// fmt.Println(y)
+
+	// var x, y = 10, "Hello"
+	// a, b := 10, "hello"
+	// fmt.Println(x)
+	// fmt.Println(y)
+	// fmt.Println(a)
+	// fmt.Println(b)
+
+	// x := 10
+	// x, y := 20, "hello"
+
+	// fmt.Println(x)
+	// fmt.Println(y)
+
+	// const a int64 = 10
+	// fmt.Println(a)
+
+	// x := 10
+	// x = 20
+	// fmt.Println(x)
+	// x = 30
+
+	// _0 := 0_0
+	// _1 := 20
+	// π := 3
+	// ａ := "hello" // Unicode U+FF41
+	// fmt.Println(_0)
+	// fmt.Println(_1)
+	// fmt.Println(π)
+	// fmt.Println(ａ)
+
+	ａ := "hello"   // Unicode U+FF41
+	a := "goodbye" // standard lowercase a (Unicode U+0061)
+	fmt.Println(ａ)
+	fmt.Println(a)
+}
